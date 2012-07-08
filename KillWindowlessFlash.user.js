9494a4cd6e893d243a4e840dbd2f62c366b035c1
James Teh <jamie@jantrid.net>
2012-07-09 09:08:16 +1000

    KillWindowlessFlash: * Add "@include *" to make this work with Scriptish, which doesn't run the script at all by default. * Reinsert a clone of the node instead of reinserting the node itself, as the latter doesn't reload Flash with the latest Firefox and Flash.

--- KillWindowlessFlash.user.js
+++ KillWindowlessFlash.user.js
@@ -3,10 +3,19 @@
 // @namespace      http://www.jantrid.net/axSGrease/
 // @description    Makes windowless (transparent or opaque) Adobe Flash objects windowed so they have a chance of being accessible.
 // @author James Teh <jamie@jantrid.net>
-// @copyright 2011 James Teh
+// @copyright 2011-2012 James Teh
 // @license GNU General Public License version 2.0
-// @version 0.20111126.02
+// @version 0.20120709.01
+// @include *
 // ==/UserScript==
+
+function reloadFlash(elm) {
+	// We need to remove the node from the document and add it again to reload Flash.
+	// In some cases, it's not sufficient to simply replace with the same node,
+	// as this seems to get optimised and does nothing.
+	// Therefore, use a clone of the node.
+	elm.parentNode.replaceChild(elm.cloneNode(), elm);
+}
 
 function killWindowlessFlash() {
 	// First, deal with embed elements.
@@ -18,9 +27,8 @@ function killWindowlessFlash() {
 		if (elm.getAttribute("wmode") == "window")
 			continue;
 		elm.setAttribute("wmode", "window");
-		// Parameters are only read when Flash is loaded,
-		// so reinsert the element to reload it.
-		elm.parentNode.replaceChild(elm, elm);
+		// Parameters are only read when Flash is loaded.
+		reloadFlash(elm);
 	}
 
 	// Now, deal with object elements.
@@ -37,9 +45,8 @@ function killWindowlessFlash() {
 			if (param.getAttribute("value") == "window")
 				continue;
 			param.setAttribute("value", "window");
-			// Parameters are only read when Flash is loaded,
-			// so reinsert the element to reload it.
-			elm.parentNode.replaceChild(elm, elm);
+			// Parameters are only read when Flash is loaded.
+			reloadFlash(elm);
 			break;
 		}
 	}
