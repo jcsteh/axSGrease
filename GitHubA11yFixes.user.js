jcsteh/axSGrease

commit 134618adda1e671b687cba60b576dbcff239cfde

GitHub: Fix table lists in issue listings, etc. broken by recent GitHub update.

GitHubA11yFixes.user.js
@@ -96,11 +96,13 @@ function onNodeAdded(target) {
 		onSelectMenuItemChanged(elem);
 	}
 	// Table lists; e.g. in issue listings.
-	for (elem of target.querySelectorAll(".table-list"))
+	for (elem of target.querySelectorAll(".Box-body"))
 		elem.setAttribute("role", "table");
-	for (elem of target.querySelectorAll(".table-list-item"))
+	for (elem of target.querySelectorAll(".d-table"))
+		elem.setAttribute("role", "presentation");
+	for (elem of target.querySelectorAll(".Box-body-row"))
 		elem.setAttribute("role", "row");
-	for (elem of target.querySelectorAll(".table-list-cell"))
+	for (elem of target.querySelectorAll(".d-table-cell"))
 		elem.setAttribute("role", "cell");
 	// Tables in Markdwn content get display: block, which causes them not to be treated as tables.
 	for (elem of target.querySelectorAll(".markdown-body table"))

commit d10c88a0bb6e9b1a17afbd292c82665540547abc

GitHub: Fix commit listing tables regressed by 134618ad.

GitHubA11yFixes.user.js
@@ -98,14 +98,14 @@ function onNodeAdded(target) {
 		elem.setAttribute("role", "menuitem");
 		onSelectMenuItemChanged(elem);
 	}
-	// Table lists; e.g. in issue listings.
-	for (elem of target.querySelectorAll(".Box-body"))
+	// Table lists; e.g. in issue and commit listings.
+	for (elem of target.querySelectorAll(".table-list,.Box-body"))
 		elem.setAttribute("role", "table");
 	for (elem of target.querySelectorAll(".d-table"))
 		elem.setAttribute("role", "presentation");
-	for (elem of target.querySelectorAll(".Box-body-row"))
+	for (elem of target.querySelectorAll(".table-list-item,.Box-body-row"))
 		elem.setAttribute("role", "row");
-	for (elem of target.querySelectorAll(".d-table-cell"))
+	for (elem of target.querySelectorAll(".table-list-cell,.d-table-cell"))
 		elem.setAttribute("role", "cell");
 	// Tables in Markdwn content get display: block, which causes them not to be treated as tables.
 	for (elem of target.querySelectorAll(".markdown-body table"))