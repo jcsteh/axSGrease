44531b99c1bd8beaf84d609c3b6354c0d3eb77c8
derekriemer <derek.riemer@colorado.edu>
2016-08-28 04:21:58 -0600

    Clean up shell script, Fix spacing in readme's markdown

--- createReadme.sh
+++ createReadme.sh
@@ -1,1 +1,6 @@
-echo "# axSGrease: Scripts for various websites" > readme.md && egrep -h "^// @(name|description|license|author|version|copyright)\s" * | sed -r "s=//\s==" | sed -r "s/@name\s+/##/" | sed -r "s/@description\s+//" | sed "s/@((copyright|author|license|version))\s+/* \1:/" -r  >> readme.md
+echo "# axSGrease: Scripts for various websites" > readme.md && 
+egrep -h "^// @(name|description|license|author|version|copyright)\s" * |
+sed -r "s=//\s==" |
+sed -r "s/@name\s+/## /" |
+sed -r "s/@description\s+//" |
+sed "s/@((copyright|author|license|version))\s+/* \1:/" -r  >> readme.md
